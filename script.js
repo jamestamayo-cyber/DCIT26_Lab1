9a3dedc50c89b085ff1189d84c433615e3220c9a
James <james.tamayo@cvsu.edu.ph>
2026-09-15 10:11:29 +0800

    Added CSS styling and JavaScript interactivity

---
+++ script.js
@@ -0,0 +1,7 @@
+// Find the button on the webpage using its ID
+const button = document.getElementById("magicButton");
+
+// Tell the button what to do when someone clicks it
+button.addEventListener("click", function() {
+    alert("🎉 Awesome! Your JavaScript interactivity is working perfectly!");
+});
